6b71cea3eb586b826620e281257afee9a1d25704
Bashmachok1982 <Bashmachok1982@gmail.com>
2026-03-02 08:36:00 +0200

    беттер

--- src/js/simple-lightbox-init.js
+++ src/js/simple-lightbox-init.js
@@ -8,9 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.windows-list-item a').forEach(link => {
     link.href = base + link.getAttribute('href');
   });
+  document
+    .querySelectorAll('.windows-list-item a, [data-lightbox="better"]')
+    .forEach(link => {
+      link.href = base + link.getAttribute('href');
+    });
 
   // потом инициализируем lightbox
   new SimpleLightbox('.windows-list-item a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    fadeSpeed: 300,
+    overlayOpacity: 0.9,
+    showCounter: true,
+  });
+  new SimpleLightbox('[data-lightbox="better"]', {
     captions: true,
     captionsData: 'alt',
     captionPosition: 'bottom',
